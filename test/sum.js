27d511a5467c357329cf3ec26cd3a35ae2678f38
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-11 17:13:08 +0300

    add tests for product sum

---
+++ test/sum.js
@@ -0,0 +1,25 @@
+var Lazy = require('lazy');
+var expresso = expresso;
+
+function range(i, j) {
+    var r = [];
+    for (;i<j;i++) r.push(i);
+    return r;
+}
+
+exports['sum'] = function (assert) {
+    var lazy = new Lazy;
+    var executed = 0;
+    lazy.sum(function (y) {
+        executed++;
+        assert.equal(y, 45);
+    })
+
+    range(0,10).forEach(function (x) {
+        lazy.emit('data', x);
+    });
+    lazy.emit('end');
+
+    assert.equal(executed, 1, 'sum failed to execute');
+}
+
