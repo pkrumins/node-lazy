ee38256efb31132b9e2c6c30c5c516a13798cbb2
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-11 03:57:04 +0300

    test for takeWhile

---
+++ test/takeWhile.js
@@ -0,0 +1,26 @@
+var Lazy = require('lazy');
+var expresso = expresso;
+
+function range(i, j) {
+    var r = [];
+    for (;i<j;i++) r.push(i);
+    return r;
+}
+
+exports['filter'] = function (assert) {
+    var lazy = new Lazy;
+    var data = [];
+    var executed = false;
+    lazy.takeWhile(function (x) { return x < 5 }).join(function (xs) {
+        assert.deepEqual(xs, range(0,5));
+        executed = true;
+    });
+
+    range(0,10).forEach(function (x) {
+        lazy.emit('data', x);
+    });
+    lazy.emit('end');
+
+    assert.ok(executed, 'join didn\'t execute');
+}
+
