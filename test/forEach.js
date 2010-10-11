edda562d3220c1a1a0155829a7d5cdd90fb993d9
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-11 04:09:44 +0300

    forEach test

---
+++ test/forEach.js
@@ -0,0 +1,30 @@
+var Lazy = require('lazy');
+var expresso = expresso;
+
+function range(i, j) {
+    var r = [];
+    for (;i<j;i++) r.push(i);
+    return r;
+}
+
+exports['filter'] = function (assert) {
+    var lazy = new Lazy;
+    var executed = false;
+    var data1 = [];
+    var data2 = [];
+    lazy
+        .forEach(function (x) {
+            data1.push(x);
+        })
+        .forEach(function (x) {
+            data2.push(x);
+        });
+
+    range(0,10).forEach(function (x) {
+        lazy.emit('data', x);
+    });
+
+    assert.deepEqual(data1, range(0,10));
+    assert.deepEqual(data2, range(0,10));
+}
+
