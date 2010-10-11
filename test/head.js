e58478c6c6afb850200790fa2b9e74067532c239
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-11 16:48:57 +0300

    test for .head

---
+++ test/head.js
@@ -0,0 +1,25 @@
+var Lazy = require('lazy');
+var expresso = expresso;
+
+function range(i, j) {
+    var r = [];
+    for (;i<j;i++) r.push(i);
+    return r;
+}
+
+exports['head'] = function (assert) {
+    var lazy = new Lazy;
+    var data = [];
+    var executed = 0;
+    lazy.head(function (x) {
+        assert.equal(x, 0);
+        executed++;
+    })
+
+    range(0,10).forEach(function (x) {
+        lazy.emit('data', x);
+    });
+
+    assert.equal(executed, 1, 'head executed too much');
+}
+
