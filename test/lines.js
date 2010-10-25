37a5d79c4b06d71b303b7248e3deaf0c4621379b
James Halliday <substack@gmail.com>
2010-10-25 13:02:22 +0000

    line test

---
+++ test/lines.js
@@ -0,0 +1,29 @@
+var Lazy = require('lazy');
+
+exports.lines = function (assert) {
+    var lazy = Lazy();
+    var joined = false;
+    lazy.lines
+        .forEach(function (line) {
+            assert.ok(line instanceof Buffer);
+            assert.ok(!line.toString().match(/\n/));
+            assert.ok(line.length > 0);
+        })
+        .join(function (lines) {
+            assert.deepEqual(
+                lines.map(function (x) { return x.toString() }),
+                'foo bar baz quux moo'.split(' ')
+            );
+            joined = true;
+        });
+    ;
+    
+    setTimeout(function () {
+        lazy.emit('data', new Buffer('foo\nbar'));
+        lazy.emit('data', new Buffer('\nbaz\nquux\nmoo'));
+        lazy.emit('data', new Buffer(''));
+        lazy.emit('data', new Buffer('\ndoom'));
+        lazy.emit('end');
+        assert.ok(joined);
+    }, 50);
+};
