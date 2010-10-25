4aaa68bb63dd3388f8ebffe5e38bdb28559a2a9a
James Halliday <substack@gmail.com>
2010-10-25 13:03:47 +0000

    emit strings, get buffers in lines test

--- test/lines.js
+++ test/lines.js
@@ -1,6 +1,6 @@
 var Lazy = require('lazy');
 
-exports.lines = function (assert) {
+exports['buffered lines'] = function (assert) {
     var lazy = Lazy();
     var joined = false;
     lazy.lines
@@ -23,6 +23,34 @@ exports.lines = function (assert) {
         lazy.emit('data', new Buffer('\nbaz\nquux\nmoo'));
         lazy.emit('data', new Buffer(''));
         lazy.emit('data', new Buffer('\ndoom'));
+        lazy.emit('end');
+        assert.ok(joined);
+    }, 50);
+};
+
+exports['string lines'] = function (assert) {
+    var lazy = Lazy();
+    var joined = false;
+    lazy.lines
+        .forEach(function (line) {
+            assert.ok(line instanceof Buffer);
+            assert.ok(!line.toString().match(/\n/));
+            assert.ok(line.length > 0);
+        })
+        .join(function (lines) {
+            assert.deepEqual(
+                lines.map(function (x) { return x.toString() }),
+                'foo bar baz quux moo'.split(' ')
+            );
+            joined = true;
+        });
+    ;
+    
+    setTimeout(function () {
+        lazy.emit('data', 'foo\nbar');
+        lazy.emit('data', '\nbaz\nquux\nmoo');
+        lazy.emit('data', '');
+        lazy.emit('data', '\ndoom');
         lazy.emit('end');
         assert.ok(joined);
     }, 50);
